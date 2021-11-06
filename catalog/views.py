a1c781493bbeb5dd354932a51701e0d0c643aeaa
AyushPaudel <ap40021319@student.ku.edu.np>
2021-11-06 23:53:22 +0545

    COmpleted author details

---
+++ catalog/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render
+from django.views import generic
+
+# Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
+
+
+# class BookListView(generic.ListView):
+#     model = Book
+#     context_object_name = 'book_list'   # your own name for the list as a template variable
+#     queryset = Book.objects.all() # Get 5 books containing the title war
+#     template_name = 'book_list.html'
+
+
+class BookListView(generic.ListView):
+    model = Book
+    template_name = 'book_list.html'
+    paginate_by = 2
+
+
+
+    def get_queryset(self):
+        return Book.objects.all()
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+
+class AuthorListView(generic.ListView):
+    model = Author
+    template_name = 'author_list.html'
+    paginate_by = 2
+
+
+
+    def get_queryset(self):
+        return Author.objects.all()
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(AuthorListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    template_name = 'author_detail.html'
