6a5c6e3b67b3dc09f1ed6dfb85f6a70e0a6ce89f
AyushPaudel <ap40021319@student.ku.edu.np>
2021-11-29 17:43:42 +0545

    changes

--- catalog/views.py
+++ catalog/views.py
@@ -30,6 +30,7 @@ def index(request):
 
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
+    print(type(num_visits))
 
     context = {
         'num_books': num_books,
