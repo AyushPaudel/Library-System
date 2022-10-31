0490ba77155f70a9339f44dc9f124f1d92f53d96
Ayush Aryal <58439228+ayush7aryal@users.noreply.github.com>
2022-10-31 16:52:35 +0545

    Update models.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from django.urls import reverse
+
+from django.urls import reverse,path
 import uuid
 from datetime import date
 
